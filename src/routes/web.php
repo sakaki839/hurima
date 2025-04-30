f5741d93dcbf17cc7b4ec27ebae052c508e39979
sakakibara akane <sakaki839@gmail.com>
2025-04-30 22:48:36 +0900

    リモートリポジトリの変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ExhibitionController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\AddressController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +20,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [ItemController::class, 'top']);
+
+Route::get('/register', [RegisterController::class, 'register']);
+
+Route::get('/login', [LoginController::class, 'login']);
+
+Route::get('/mypage', [ItemController::class, 'mypage']);
+
+Route::get('/mypage/profile', [ProfileController::class, 'profile']);
+
+Route::get('/item', [ItemController::class, 'item']);
+
+Route::get('/purchase/address/:item_id', [AddressController::class, 'item_id']);
+
+Route::get('/purchase/:item_id', [PurchaseController::class, 'item_id1']);
+
+Route::get('/sell', [ItemController::class, 'sell']);
+
+Route::get('/item/:item_id', [ItemController::class, 'item_id2']);
+
